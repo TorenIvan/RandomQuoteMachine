ed313e7bcfb79e1a1c28434dda0ccbe2edf6f28e
Toren Ivan <Vaggelisshmos@gmail.com>
2021-03-04 21:21:13 +0200

    The beggining is always something else's end

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Footer extends React.Component{
+
+}
+
+class Quote extends React.Component{
+
+}
+
+class Wrapper extends React.Component{
+    render() {
+        return (
+            <body className="page">
+
+            </body>
+        );
+    }
+}
+
+class Machine extends React.Component{
+    render(){
+        return <wrapper/>
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Machine />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
