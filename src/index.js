eeb7a2b04a76f6042eeb1c78e9a0c1734ee4b8d0
Toren Ivan <Vaggelisshmos@gmail.com>
2021-03-21 16:21:31 +0200

    changeColor added

--- src/index.js
+++ src/index.js
@@ -3,40 +3,63 @@ import ReactDOM from 'react-dom'
 import './index.css';
 
 
+const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+
 function NewQuoteButton(props){
     return(
         <button 
-            id="new-quote"
-            onClick={props.onClick}
+            id = "new-quote"
+            onClick = {props.onClick}
+            style = {{backgroundColor: props.value}}
         >
             <p>New Quote</p>
         </button>
     );
 }
 
-function TumblerButton(){
+function TumblerButton(props){
         return(
-            <button id="tumblr-quote">
-                <a href='https://twitter.com/share'ref='share' data-url='https://twitter.com/'>Tweet</a>
+            <button 
+                id = "tumblr-quote"
+                style = {{backgroundColor: props.value}}    
+            >
+                {/* <a href='https://twitter.com/share'ref='share' data-url='https://twitter.com/'>Tweet</a> */}
             </button>
         );
 }
 
-function TwitterButton(){
+function TwitterButton(props){
     return(
-        <button id="tumblr-quote">
-            <a href='https://twitter.com/intent/tweet' ref='share' data-url='https://twitter.com/'>Tweet</a>
+        <button 
+            id = "tumblr-quote"
+            style = {{backgroundColor: props.value}}
+        >
+            {/* <a href='https://twitter.com/intent/tweet' ref='share' data-url='https://twitter.com/'>Tweet</a> */}
         </button>
     );
 }
 
 
-class QuoteBox extends React.Component{
+class RQMachine extends React.Component{
     constructor(){
         super();
         this.state = {
             author: '',
             quote: '',
+            color: '',
         };
         this.handleClick = this.handleClick.bind(this);     //bind handleClick to "this" class (you can also use arrow function)
         this.handleClick();                                 //call the first time it loads first time load
@@ -45,15 +68,18 @@ class QuoteBox extends React.Component{
     //get a random quote and pass the method as prop to New Quote 
     handleClick(){
         let randomQuote;
+        let randomColor;
         fetch('https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
         .then(response => response.json())
         .then(data => {
             randomQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
+            randomColor = colors[Math.floor(Math.random() * colors.length)];
             this.setState({
                 author: randomQuote.author,
                 quote:  randomQuote.quote,
+                color: randomColor,
             });
-            console.log(randomQuote);
+            // console.log(randomQuote);
         })
         .catch(error => console.log(error))
     }
@@ -61,27 +87,22 @@ class QuoteBox extends React.Component{
     /* function components, due to only render method */
     render(){
         return (
-            <div id="quote-box">
-                <blockquote id="quote">
-                    <span >
-                        {this.state.quote}
-                    </span>
-                    <em> ~{this.state.author} </em>
-                </blockquote>
-                <div className="buttons">
-                    <TwitterButton/>                            
-                    <TumblerButton/>
-                    <NewQuoteButton onClick={this.handleClick}/>
+            <body style={{backgroundColor: this.state.color}}>
+                <div id="quote-box" style={{color: this.state.color}}>
+                    <blockquote id="quote">
+                        <span >
+                            {this.state.quote}
+                        </span>
+                        <em> ~{this.state.author} </em>
+                    </blockquote>
+                    <div className="buttons">
+                        <TwitterButton value={this.state.color}/>                            
+                        <TumblerButton value={this.state.color}/>
+                        <NewQuoteButton onClick={this.handleClick} value={this.state.color}/>
+                    </div>
                 </div>
-            </div>
+            </body>
         );
-    }
-}
-
-// whole page component, no need but wth
-class RQMachine extends React.Component{
-    render(){
-        return <QuoteBox/>;
     }
 }
 
@@ -92,28 +113,3 @@ ReactDOM.render(
 
 
 
-/*onClick event handler to a functional component*/
-
-//The event handler is passed as an attribute to an element or component. 
-//This attribute receives a function that describes what happens when the user interacts with the element. 
-
-//The onClick{exampleFunction()}    returns the result of exampleFunction   (only one time)
-//The onClick{exampleFunction}      calls the function                      (as many times as you call the function)
-
-
-
-/*onClick event handler to a class component*/
-
-//Similar to functions
-
-//The onClick{exampleMethod()}    returns the result of exampleMethod   (only one time)
-//The onClick{exampleMethod}      calls the Method                      (as many times as you call the function)
-
-/*On the above, what about when you want to call a method/function (with onClick{example}) but you want to pass parameters*/
-//Answer:   onClick(() => example(parameter))           //on functions or classes
-//          binding in constructor                      //on classes
-
-
-/* TO-DO's */
-//Buttons to tweet it
-//On first try quote something
